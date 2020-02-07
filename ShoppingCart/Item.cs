4d0431eaee6631ef0b0d2f538cc01bbb80163adf
Deep <deekay1410@gmail.com>
2020-02-07 12:32:12 -0500

    Created properties for item class

---
+++ ShoppingCart/Item.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart
+{
+    class Item
+    {
+        public string Id { get; set; }
+
+        private string _description;
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (value == "" || value.Length <= 200)
+                    throw new Exception("Description cannot be empty or exceed 200 characters");
+                _description = value;
+
+            }
+        }
+
+        private double _price;
+
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if 
+                _price = value;
+            }
+        }
+
+    }
+}
