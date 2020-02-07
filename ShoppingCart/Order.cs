2fd84dbec9a9d6cc0413d74ef7fae797734965ba
Oliver Drenth <oliverdrenth@gmail.com>
2020-02-07 12:40:39 -0500

    Did item class properties

---
+++ ShoppingCart/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart
+{
+    class Order
+    {
+        enum Status { Received, InProgress, Delivered}
+        public DateTime Date { get; set; }
+
+        Item Items = new Item();
+
+        public double CalculateTotal() 
+        {
+
+        }
+    }
+}
